2a46779ae78db9993d9c83d36421e979d83b60e7
Yeferson Heredia <142361283+Yefersom10@users.noreply.github.com>
2024-11-03 17:40:50 -0500

    Delete Bakend_TaskManager directory

--- Bakend_TaskManager/src/main/java/com/anonimo/Bakend_TaskManager/repository/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anonimo.Bakend_TaskManager.repository;
-
-import com.anonimo.Bakend_TaskManager.entity.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TareaRepository extends JpaRepository<Tarea, Integer> {
-}
